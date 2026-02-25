cc4debaa1667a17d20cdc8547070eba6692d53d2
vansh-gg <vansh1234g@gmail.com>
2026-02-25 14:05:25 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        // Displaying welcome message and version as per requirements
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Application Version: 1.0");
+        System.out.println("---------------------------------------------------");
+    }
 }
