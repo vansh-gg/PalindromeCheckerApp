38ba505da6dd2115406c934d42cc7bec0f64fa9b
vansh-gg <vansh1234g@gmail.com>
2026-02-25 14:40:56 +0530

    UC13 - Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,12 +1,20 @@
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
         PalindromeStrategy strategy = new StackStrategy();
-        String testInput = "racecar";
+        String testInput = "level";
 
+        long startTime = System.nanoTime();
         boolean isPalindrome = strategy.check(testInput);
-        System.out.println("Result: " + isPalindrome);
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+
+        System.out.println("Input : " + testInput);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + duration + " ns");
     }
 }
 
@@ -19,7 +27,50 @@ class StackStrategy implements PalindromeStrategy {
     public boolean check(String input) {
         if (input == null) return false;
 
-        Stack<Character> stack = new Stack<>();
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}import java.util.Stack;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        PalindromeStrategy strategy = new StackStrategy();
+        String testInput = "level";
+
+        long startTime = System.nanoTime();
+        boolean isPalindrome = strategy.check(testInput);
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+
+        System.out.println("Input : " + testInput);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + duration + " ns");
+    }
+}
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        if (input == null) return false;
+
+        java.util.Stack<Character> stack = new java.util.Stack<>();
 
         for (char c : input.toCharArray()) {
             stack.push(c);
